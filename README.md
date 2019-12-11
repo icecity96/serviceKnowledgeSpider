c3f1f7cabd8202621b359cb7ff39959b2bd66284
MingyiLiu <liumy@hit.edu.cn>
2019-12-11 13:48:56 +0800

    update

--- README.md
+++ README.md
@@ -7,13 +7,35 @@ This project is used to collect service-related entities information to build a 
 You are welcome to send a pull request or open an issue.
 
 ## Resources List
-* **pedaily_enterprise**
-    * URL: https://zdb.pedaily.cn/enterprise/
-    * fn: enterprise full name (e.g., 阿里巴巴集团控股有限公司)
-    * sn: enterprise short name (e.g., 阿里巴巴)
-    * loc: enterprise location (e.g., 余杭区)
-    * reg: enterprise registration location (e.g., 余杭区)
-    * cre: enterprise creation date (e.g., 1999年)
-    * dom: enterprise domain (e.g., B2B)
-    * web: enterprise web site (e.g., www.alibabagroup.com)
-    * intro: enterprise brief introduction (e.g.,阿里巴巴集团控股有限公司是一家电子商务企业，旗下拥有淘宝，天猫，聚划算，1688等多个品牌。集团经营多元化的互联网业务，致力为全球所有人创造便捷的交易渠道。自成立以来，阿里巴巴集团建立了消费者电子商务、网上支付、B2B网上交易市场及云计算业务，近几年更积极开拓无线应用、手机操作系统和互联网电视等领域。集团以促进一个开放、协同、繁荣的电子商务生态系统为目标，旨在对消费者、商家以及经济发展做出贡献。阿里巴巴是主要从事电子商务，是网上交易市场和商务交流社区，融合了B2B、C2C、搜索引擎和门户。阿里巴巴两次入选哈佛大学商学MBA案例，在美国学术界掀起研究热潮；连续五次被美国权威财经杂志《福布斯》选为全球最佳B2B站点之一；多次被相关机构评为全球最受欢迎的B2B网站、中国商务类优秀网站、中国百家优秀网站、中国最佳贸易网。被国内外媒体、硅谷和国外风险投资家誉为与Yahoo， Amazon， eBay，AOL比肩的五大互联网商务流派代表之一。全球互联网流量监测网站Alexa.com对全球商务及贸易类网站进行排名调查，阿里巴巴网站排名首位。调查显示：阿里巴巴的网上会员近五成是通过口碑相传得知阿里巴巴并使用阿里巴巴；各行业会员通过阿里巴巴商务平台双方达成合作者占总会员比率近五成。 )
+* **pedaily_enterprise**: https://zdb.pedaily.cn/enterprise
+    
+    | column | explanation |
+    | ----| -------------------------------- |
+    | fn | enterprise full name (e.g., 阿里巴巴集团控股有限公司) |
+    | sn | enterprise short name (e.g., 阿里巴巴) |
+    | loc | enterprise location (e.g., 余杭区) |
+    | reg | enterprise registration location (e.g., 余杭区) |
+    | cre | enterprise creation date (e.g., 1999年) |
+    | dom | enterprise domain (e.g., B2B) |
+    | web | enterprise web site (e.g., www.alibabagroup.com) |
+    | intro| enterprise brief introduction (e.g.,阿里巴巴集团控股有限公司是一家电子商务企业，旗下拥有... )|
+    
+* **mi_appstore**: http://app.mi.com/
+
+    | column | explanation | example |
+    | --- | -------------- | ------- |
+| fn | app full name | 中国移动 |
+| dev | developer | 中国移动通信有限公司 |
+| url | url | http://app.mi.com/details?id=com.greenpoint.android.mc10086.activity |
+| ca | app category | 实用工具 |
+| sp | support platform | 手机 |
+| avs | app average score | 3 |
+| cn | comment number | 7519 |
+| size | app size | 34.32 M |
+| v | app version | 5.8.2 |
+| ut | update time | 2019-11-30|
+| pn | package name | com.greenpoint.android.mc10086.activity |
+| id | app ID | 51733 |
+| pm | app permissions | 蓝牙管理-录音-...|
+| intro | app introduction | .... |
+| new | what's new | ... |
---
+++ mi_appstore.py
@@ -0,0 +1,141 @@
+import requests
+from bs4 import BeautifulSoup
+import json
+
+class appInfo(object):
+
+    def __init__(self):
+        self.url = ""
+        self.fn = ""
+        self.dev = ""
+        self.ca = ""
+        self.sp = ""
+        self.avs = ""
+        self.cn = ""
+        self.size = ""
+        self.v = ""
+        self.ut = ""
+        self.pn = ""
+        self.id = ""
+        self.pm = ""
+        self.intro = ""
+        self.new = ""
+
+    def __str__(self):
+        s = "{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\n".format(
+            self.url, self.fn, self.dev, self.ca, self.sp, self.avs, self.cn,
+            self.size, self.v, self.ut, self.pn, self.id, self.pm, self.intro, self.new
+        )
+        return s
+
+def spider(url):
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36',
+        'Upgrade-Insecure-Requests': '1'
+    }
+    response = requests.get(url, headers=headers)
+    if response.status_code != 200: # this means that this category has spider over
+        return False, None
+    app_list = ""
+    try:
+        app_list = response.json()['data']
+    except:
+        raise Exception(url + response.text)
+    app_result = []
+    for app in app_list:
+        app_info = appInfo()
+        packageName = app["packageName"]
+        app_info.pn = packageName
+        app_info.id = app["appId"]
+        app_info.url = "http://app.mi.com/details?id={}".format(packageName)
+        response = requests.get(app_info.url, headers=headers)
+        soup = BeautifulSoup(response.text, 'lxml')
+        intro_titles = soup.find('div', class_='intro-titles')
+        try:
+            app_info.dev = intro_titles.find('p').text
+        except:
+            pass
+        try:
+            app_info.fn = intro_titles.find('h3').text
+        except:
+            pass
+        try:
+            special_font = intro_titles.find('p', class_='sepcial-font').text
+            special_font = special_font.split('|')
+            app_info.ca, app_info.sp = special_font[0][3:], special_font[1][3:]
+        except:
+            pass
+        try:
+            star1 = intro_titles.find('div', class_='star1-empty').find('div')['class'][1]
+            app_info.avs = star1.split('-')[-1]
+            app_intro_comments = intro_titles.find('span', class_='app-intro-comment').text
+            app_info.cn = ''.join([c for c in app_intro_comments if str(c).isdigit()])
+        except:
+            pass
+
+        look_detail = soup.find('div', class_='look-detail')
+        try:
+            cf = look_detail.find('ul', class_='cf')
+            which = ""
+            for li in cf.children:
+                if li is None:
+                    continue
+                if '软件大小' in li.text:
+                    which = "size"
+                elif '版本' in li.text:
+                    which = "version"
+                elif '更新' in li.text:
+                    which = "update"
+                elif which == "size":
+                    app_info.size = li.text
+                    which = ""
+                elif which == "version":
+                    app_info.v = li.text
+                    which = ""
+                elif which == "update":
+                    which = ""
+                    app_info.ut = li.text
+        except:
+            pass
+
+        permissions = []
+        try:
+            for li in look_detail.find('ul', class_='second-ul').children:
+                permissions.append(li.text[1:].strip())
+        except:
+            pass
+        app_info.pm = '-'.join(permissions)
+        app_text = soup.find('div', class_='app-text')
+        if app_text is not None:
+            app_text = app_text.find_all('p', class_='pslide')
+            if app_text is not None and len(app_text) > 0:
+                app_info.intro = app_text[0].text.replace('\t', ' ').replace('\n', ' ').replace('\r', ' ')
+            if len(app_text) > 1:
+                app_info.new = app_text[1].text.replace('\t', ' ').replace('\n', ' ').replace('\r', ' ')
+        app_result.append(app_info)
+
+    return True, app_result
+
+
+if __name__ == '__main__':
+    categorys = [5, 27, 2, 7, 12, 10, 9, 4, 3, 6, 14, 8, 11, 13, 1, 15]
+    data_file = "/home/lmy/Project/CornJobs/serviceknowledgespider/data/mi_appstore.entities"
+    for i in range(2):
+        lines = open('/home/lmy/Project/CornJobs/serviceknowledgespider/mi_appstore.cfg', 'r').readlines()
+        lines = [int(line.strip()) for line in lines]
+        if lines[0] >= len(categorys):
+            exit(-1)
+        page, cid = lines[1], categorys[lines[0]]
+        url = 'http://app.mi.com/categotyAllListApi?page={:d}&categoryId={:d}&pageSize=30'.format(page, cid)
+        ok, res = spider(url)
+        with open('/home/lmy/Project/CornJobs/serviceknowledgespider/mi_appstore.cfg', 'w') as w:
+            if len(res) > 0:
+                w.write("{:d}\n".format(lines[0]))
+                w.write("{:d}\n".format(page+1))
+            else:
+                w.write("{:d}\n".format(lines[0]+1))
+                w.write("{:d}\n".format(0))
+        if len(res) == 0:
+            continue
+        with open(data_file, 'a') as w:
+            w.writelines([r.__str__() for r in res])
